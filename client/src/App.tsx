78972bf5750e18a974020f35efefaa269ff9155c
unknown <davit.khachaturov.1@iliauni.edu.ge>
2023-11-25 23:57:15 +0400

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [test, setTest] = useState<any>({});
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        greeting: "Hello World!"
+      }),
+    })
+      .then(res => res.json())
+      .then(json => setTest(json))
+
+  }, [])
+
+  console.log(test);
+
 
   return (
     <>
+      {test.greeting}
     </>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,10 +8,8 @@ app.use(cors());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/api', (req: any, res: any) => {
-  res.json({
-    Hello: 'World'
-  })
+app.post('/api', (req: any, res: any) => {
+  res.json(req.body)
 })
 
 app.listen(PORT, () => {
