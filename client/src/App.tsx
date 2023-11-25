54d0d86783d8c87db8f7b14f417b05cedcc7543e
unknown <davit.khachaturov.1@iliauni.edu.ge>
2023-11-26 02:50:55 +0400

    made a first sketch of app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,52 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-
+interface IrowData {
+  attributes: {
+    [key: string]: number | string;
+  }
+}
 function App() {
-  const [test, setTest] = useState<any>({});
+  const [file, setFile] = useState<File | null>(null);
+  const [data, setData] = useState<IrowData[] | null>(null);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        greeting: "Hello World!"
-      }),
-    })
-      .then(res => res.json())
-      .then(json => setTest(json))
+  const handlePhotoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) setFile(file);
+  }
 
-  }, [])
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  console.log(test);
+    if (file) {
+      const formData = new FormData();
+      formData.append('htmlFile', file);
+      fetch('http://localhost:3000/api', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(res => res.json())
+        .then(json => setData(json))
+    }
+  }
+
+  console.log(file, data);
 
 
   return (
     <>
-      {test.greeting}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="file" accept="text/html" onChange={handlePhotoChange} />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+      <ul>
+        {data && data.map((item: IrowData) => <li key={item.attributes.Name}>
+          <span>{item.attributes.Name}</span>
+          <span>{item.attributes.Age}</span>
+          <span>{item.attributes['Transfer Value']}</span>
+        </li>)}
+      </ul>
     </>
   )
 }
